23accd4b00c7cb5dadc55ca1f412d9940de51e23
colbyschulz <colbyschulz@gmail.com>
2016-10-03 23:20:01 -0400

    Added signup component aand action - new users persist to API db

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import NavBar from './nav_bar'
+import SignUp from './sign_up'
 
 class App extends Component {
-  constructor(){
-    super()
-  }
   render() {
     return (
       <div className="App">
         <NavBar />
         <div className="container">
           <h1> Yes</h1>
+          <SignUp />
           {this.props.children}
         </div>
       </div>
