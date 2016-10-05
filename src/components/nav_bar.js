3d82cf2d357cdf0ff70cde28aebefb876115658d
colbyschulz <colbyschulz@gmail.com>
2016-10-05 17:22:29 -0400

    Complete Signup process and add user show page

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -6,7 +6,7 @@ export default function NavBar(props){
     <nav className='navbar navbar-inverse'>
       <div className='navbar-header'>
         <Link to="/" className='navbar-brand'>Magic The Gathering: The Gathering</Link>
-        <Link to="/signup">Sign Up</Link>
+        <Link to="/signup" className='navbar-item'>Sign Up</Link>
 
       </div>
     </nav>
