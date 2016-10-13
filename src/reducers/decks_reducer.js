7e02eb4bf1423a489c29c8b88b6516e7af0d84e9
colbyschulz <colbyschulz@gmail.com>
2016-10-13 15:09:35 -0400

    add remove card to collection and deck

--- src/reducers/decks_reducer.js
+++ src/reducers/decks_reducer.js
@@ -7,6 +7,14 @@ export default function decks_reducer(state=[], action){
 			copyState.splice(index,1,action.payload)
 			console.log("add to deck reducer", copyState)
 			return copyState
+		case 'REMOVE_CARD_FROM_DECK':
+			const deckRemove = state.find(deck => deck.id == action.payload.id)
+			console.log("begin reducer action", deckRemove)
+			const copyStateRemove = [...state]
+			const indexRemove = copyStateRemove.indexOf(deckRemove)
+			copyStateRemove.splice(indexRemove,1,action.payload)
+			console.log("end reducer action", copyStateRemove)
+			return copyStateRemove
 		case 'FETCH_DECKS':
 			return action.payload
 		default:
